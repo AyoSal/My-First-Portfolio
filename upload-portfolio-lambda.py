8611e9003b5bbcc397a6005a6650fb250d080b2b
Ayo Salawu <ayosalawu@gmail.com>
2017-09-11 20:27:55 +0400

    Update portfolio deployment function for Lamda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,19 +2,21 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.ayosal.info')
-build_bucket = s3.Bucket('portfoliobuild.ayosal.info')
+    portfolio_bucket = s3.Bucket('portfolio.ayosal.info')
+    build_bucket = s3.Bucket('portfoliobuild.ayosal.info')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj, nm,
-         ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Done!"
+    return 'Hello from Lambda'
